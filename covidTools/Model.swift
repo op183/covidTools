a2dd262e022160a8276ed9a67a70bc1679620cbb
Ivo Vacek <ivo.vacek@gmail.com>
2020-07-15 10:50:14 +0300

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -111,9 +111,9 @@ class SHIRJA: ObservableObject {
                 refDay + 47:0.215,        // prva faza uvolnenia
                 refDay + 61:0.23,       // druha a tretia faza uvolnenia
                 refDay + 75:0.33,        // stvrta faza uvolnenia
-                refDay + 90:0.75,        // piata faza uvolnenia ??
+                refDay + 90:0.72,        // piata faza uvolnenia ??
                 
-                refDay + 130:0.3,        // další lockdown ??? asi áno ...
+                refDay + 140:0.3,        // další lockdown ??? asi áno ...
 
 
             ], dist: betad)
