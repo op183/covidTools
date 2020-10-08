17ac83c4956db2eff02b926e5080e925be1e77f6
Ivo Vacek <ivo.vacek@gmail.com>
2020-10-08 10:23:43 +0200

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -118,7 +118,7 @@ class SHIRJA: ObservableObject {
                 refDay + 120:0.3,
                 refDay + 130:0.479,
                 
-                refDay + 185:0.65,
+                refDay + 185:0.63,
 
                 refDay + 210:0.35, // (new messures) standby for vaccination ???? 
 
