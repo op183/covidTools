ca373eadb6ad1d1042e957952d0885adc1b57bd7
Ivo Vacek <ivo.vacek@gmail.com>
2020-11-05 10:29:38 +0100

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -125,7 +125,7 @@ class SHIRJA: ObservableObject {
                 refDay + 220:0.43, // hromadné podujatia, reštaurácie, fitness
                 
                 refDay + 230:0.39, // lockdown + plošné testovanie (Orava, Bardejov ...) ???????
-                refDay + 237:0.33, // lockdown + plošné testovanie (plošné) ???????
+                refDay + 237:0.36, // lockdown + plošné testovanie (plošné) ???????
                 //refDay + 244:0.35, // lockdown + plošné testovanie (plošné) ???????
 
             ], dist: betad)
