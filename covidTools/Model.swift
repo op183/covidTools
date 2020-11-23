ad050a0492e3027e4b6c31d9d611c14e3e9dd143
Ivo Vacek <ivo.vacek@gmail.com>
2020-11-23 10:34:18 +0100

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -132,10 +132,10 @@ class SHIRJA: ObservableObject {
                 refDay + 245:0.2, // plošné testovanie (limitované len pre sever, čo je totálny fail!!) ???????
                 refDay + 247:0.39, // plošné testovanie (limitované len pre sever, čo je totálny fail!!) ???????
 
-                refDay + 255:0.43, // partial easing
+                refDay + 255:0.45, // partial easing
                 
-                refDay + 260:0.33,   // mass testing 4 in row (500 MOM >1%)
-                refDay + 262:0.39,
+                refDay + 260:0.37,   // mass testing 4 in row (500 MOM >1%)
+                refDay + 262:0.42,  // balanced Rt ≈ 1
 
 
             ], dist: betad)
