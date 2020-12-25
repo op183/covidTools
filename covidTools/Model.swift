f6c8a6be434e0ef7ee0db1bb7a43c7bc8c039d32
Ivo Vacek <ivo.vacek@gmail.com>
2020-12-25 12:21:27 +0300

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -136,7 +136,7 @@ class SHIRJA: ObservableObject {
                 refDay + 255:0.46, // partial easing, balanced Rt ≈ 1,1
                 
                 refDay + 260:0.41,  // "mass" testing 4th in row (500 MOM >1%), almost for nothing!!!
-                refDay + 262:0.56,  // balanced Rt ≈ 1,34 (semms to be too optimistic ?, will see in few days)
+                refDay + 262:0.53,  // revised Rt ≈ 1,29 ??? // balanced Rt ≈ 1,34 (semms to be too optimistic ?, will see in few days)
                 
                 refDay + 290:0.215, // lockdown + curfew
 
