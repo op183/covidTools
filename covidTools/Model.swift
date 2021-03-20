093fc0ceda18f870205450e26bc6b9239cd4d07c
Ivo Vacek <ivo.vacek@gmail.com>
2021-03-20 17:54:33 +0100

    daily data

--- covidTools/Model.swift
+++ covidTools/Model.swift
@@ -148,7 +148,7 @@ class SHIRJA: ObservableObject {
                 refDay + 322:0.44,
                 refDay + 341:0.46,
                 
-                refDay + 367:0.4,
+                refDay + 360:0.4,
 
             ], dist: betad)
             let b = S[i] * H[i] * l
